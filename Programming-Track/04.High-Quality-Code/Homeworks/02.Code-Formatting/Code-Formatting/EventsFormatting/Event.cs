2eb070b87e1052f193668eec5c8786764f140033
packo0 <pdimitrov1389@gmail.com>
2015-08-14 02:13:52 +0300

    added homework code-formatting and solutions to it

---
+++ Programming-Track/04.High-Quality-Code/Homeworks/02.Code-Formatting/Code-Formatting/EventsFormatting/Event.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+public class Event : IComparable
+{
+    private DateTime date;
+    private string title;
+    private string location;
+
+    public Event(DateTime date, string title, string location)
+    {
+        this.date = date;
+        this.title = title;
+        this.location = location;
+    }
+
+    public int CompareTo(object obj)
+    {
+        Event other = obj as Event;
+        int date = this.date.CompareTo(other.date);
+        int title = this.title.CompareTo(other.title);
+
+        int location = this.location.CompareTo(other.location);
+        if (date == 0)
+        {
+            if (title == 0)
+            {
+                return location;
+            }
+            else
+            {
+                return title;
+            }
+        }
+        else
+        {
+            return date;
+        }
+    }
+
+    public override string ToString()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.Append(this.date.ToString("yyyy-MM-ddTHH:mm:ss"));
+        stringBuilder.Append(" | " + this.title);
+        if (this.location != null && this.location != string.Empty)
+        {
+            stringBuilder.Append(" | " + this.location);
+        }
+
+        return stringBuilder.ToString();
+    }
+}
